0453d68daf357ad9be54c45ef705822e54e0e5e0
Keegan Leitz <keegan@openbay.com>
2019-11-21 19:20:43 -0500

    esnext and dom libs for typescript

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import invert from './invert';
 const foo: string = 'wait, dude'; // eslint-disable-line @typescript-eslint/no-inferrable-types
 
 console.log(`${foo}... is that ${invert(false)}?`);
+
+console.log(Object.assign({}, { hi: 'there' }));
